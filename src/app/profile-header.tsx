0126947c41ddd26ad35cfb04b3c012048b7b705f
Hector Alvarez <alvarez.hectorm@outlook.com>
2023-07-17 05:51:43 -0500

    added bio section to header

--- src/app/profile-header.tsx
+++ src/app/profile-header.tsx
@@ -12,19 +12,27 @@ async function getProfileData() {
 export async function ProfileHeader() {
   const profile = await getProfileData()
   return (
-    <header className="flex py-8 px-2 justify-center items-center w-full">
-      <div>
-        <Image
-          src={profile.avatar_url}
-          alt={profile.name}
-          width={75}
-          height={75}
-          className="rounded-full"
-        />
+    <header className="flex flex-col py-8 px-2 justify-center items-center w-full">
+      <div className="flex justify-center items-center">
+        <div>
+          <Image
+            src={profile.avatar_url}
+            alt={profile.name}
+            width={75}
+            height={75}
+            className="rounded-full"
+          />
+        </div>
+        <div className="pl-4">
+          <h1 className="text-2xl font-bold font-serif">{profile.name}</h1>
+          <p className="text-sm">{profile.company}</p>
+        </div>
       </div>
-      <div className="pl-4">
-        <h1 className="text-2xl font-bold font-serif">{profile.name}</h1>
-        <p className="text-sm">{profile.company}</p>
+      <div className="py-4 max-w-">
+        <h2>{profile.bio}</h2>
+        <a href={profile.html_url} className="text-sm underline text-blue-700">
+          checkout my github
+        </a>
       </div>
     </header>
   )
