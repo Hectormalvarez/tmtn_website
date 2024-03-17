e0404aa4f69d82e28d1e28a857bccb641db4a4f1
Hector Alvarez <alvarez.hectorm@outlook.com>
2024-03-17 19:22:37 +0000

    added header \ prettier + eslint rules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-5xl font-bold">Hello World</h1>
-      <p className="mt-3 text-2xl">
-        This is a Next.js app with Tailwind CSS.
-      </p>
+      <p className="mt-3 text-2xl">This is a Next.js app with Tailwind CSS.</p>
     </main>
-  )
+  );
 }
